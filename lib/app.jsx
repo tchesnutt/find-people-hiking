a7ccc49bc4a9d7215ecccc1236528bb75070fdc9
Taylor Chesnutt <taylor.chesnutt@gmail.com>
2016-12-21 20:07:11 -0800

    reorganization of directory

---
+++ lib/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Redux from 'redux';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
+import Root from './components/root';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+  let store = {};
+  const preloadedState = {};
+  ReactDOM.render(<h1>TEST</h1>);
+});
---
+++ lib/components/root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+const Root = ({ store }) => {
+
+
+  return (
+
+      <div>
+        <h1>THE</h1>
+      </div>
+
+  );
+};
+
+export default Root;
